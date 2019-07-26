456dec7121d57741328f83e396939904f5a1aa70
libing <1638123351@qq.com>
2019-07-26 18:22:13 +0800

    测试

---
+++ src/index.java
@@ -0,0 +1,10 @@
+/**
+ * @Author libin
+ * @Version v 1.0
+ * @Date 2019/7/26
+ */
+public class index {
+    public static void main(String[] args) {
+        System.out.println("hahah");
+    }
+}
